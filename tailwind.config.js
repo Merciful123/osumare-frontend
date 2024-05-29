0df2b351b2f5ceb17706f469088cf75b4433b07d
Aamir Raza <merciful9795@gmail.com>
2024-05-29 16:19:19 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+import { nextui } from "@nextui-org/react";
+
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    // ...
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+};
+
+export default config;
